3e1fcf57f292f58d9b583b972dbf82d4676a12d1
Violet Middlekauff <lucasmiddlekauff@gmail.com>
2024-04-26 13:04:03 -0500

    Add files via upload

--- appFrontend.js
+++ appFrontend.js
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "react";
+
 function App() {
   const [product, setProduct] = useState([]);
-
-  //holds the product that will be returned when
-  //a user searches for a product by id
   const [oneProduct, setOneProduct] = useState([]);
-
-  // new Product to add with user form data
+  const [viewer1, setViewer1] = useState(false);
+  const [viewer2, setViewer2] = useState(false);
+  // new Product
   const [addNewProduct, setAddNewProduct] = useState({
     id: 0,
-    title: "",
+    name: "",
     price: 0.0,
     description: "",
     category: "",
-    image: "",
+    imageUrl: "",
     rating: 0.0,
   });
 
   useEffect(() => {
     getAllProducts();
   }, []);
+
+  //function to get all the products
   function getAllProducts() {
     fetch("http://localhost:8081/listProducts")
       .then((response) => response.json())
@@ -28,8 +29,37 @@ function App() {
         console.log(data);
         setProduct(data);
       });
+    setViewer1(!viewer1);
   }
 
+  //function to get one product by its id
+  function getOneProduct(id) {
+    console.log(id);
+    if (id >= 1 && id <= 20) {
+      fetch("http://localhost:8081/product/" + id)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Show one product :", id);
+          console.log(data);
+          setOneProduct(Array.isArray(data) ? data : [data]);
+        });
+      if (false === viewer2) setViewer2(true);
+    } else {
+      console.log("Wrong number of Product id.");
+    }
+  }
+  //maps a single item in the data base
+  const showOneItem = oneProduct.map((el) => (
+    <div key={el.id}>
+      <img src={el.imageUrl} width={30} alt="images" /> <br />
+      Title: {el.name} <br />
+      Category: {el.category} <br />
+      Price: {el.price} <br />
+      Rating: {el.rating} <br />
+    </div>
+  ));
+
+  //maps all the items in the database
   const showAllItems = product.map((el) => (
     <div key={el.id}>
       <img src={el.imageUrl} width={30} alt="images" /> <br />
@@ -46,10 +76,21 @@ function App() {
       <div>
         <h3>Show all available Products.</h3>
         <button onClick={() => getAllProducts()}>Show All ...</button>
-        {showAllItems}
+        {viewer1 && showAllItems}
+      </div>
+      <div>
+        <h3>Show one Product by Id:</h3>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          placeholder="id"
+          onChange={(e) => getOneProduct(e.target.value)}
+        />
+        {viewer2 && showOneItem}
       </div>
     </div>
-  ); // return end
+  );
 }
 
 export default App;
--- backend.js
+++ backend.js
@@ -19,6 +19,7 @@ app.listen(port, () => {
   console.log("App listening at http://%s:%s", host, port);
 });
 
+//get all products
 app.get("/listProducts", async (req, res) => {
   await client.connect();
   console.log("Node connected successfully to GET MongoDB");
@@ -31,4 +32,44 @@ app.get("/listProducts", async (req, res) => {
   console.log(results);
   res.status(200);
   res.send(results);
+});
+
+//get a product by its id
+app.get("/product/:id", async (req, res) => {
+  const productid = Number(req.params.id);
+  console.log("product to find :", productid);
+  await client.connect();
+  console.log("Node connected successfully to GET-id MongoDB");
+  const query = { id: productid };
+  const results = await db.collection("products").findOne(query);
+  console.log("Results :", results);
+  if (!results) res.send("Not Found").status(404);
+  else res.send(results).status(200);
+});
+
+//adding a product
+app.post("/product/addProduct", async (req, res) => {
+  try {
+    await client.connect();
+
+    const keys = Object.keys(req.body);
+    const values = Object.values(req.body);
+
+    const newDocument = {
+      id: values[0], // also "id": req.body.id,
+      name: values[1], // also "name": req.body.name,
+      price: values[2], // also "price": req.body.price,
+      description: values[3], // also "description": req.body.description,
+      imageUrl: values[4], // also "imageUrl": req.body.imageUrl
+    };
+    console.log(newDocument);
+
+    const results = await db.collection("products").insertOne(newDocument);
+
+    res.status(200);
+    res.send(results);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).send({ error: "An internal server error occurred" });
+  }
 });
